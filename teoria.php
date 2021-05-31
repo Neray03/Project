015f7d55636ed42c766e9101cd5ddcae2f8063cf
Neray03 <82361112+Neray03@users.noreply.github.com>
2021-05-31 18:17:35 +0300

    Create teoria.php
    Страница с теоретическим материалом

---
+++ teoria.php
@@ -0,0 +1,6 @@
+<?php require_once("header.php");?>
+<div class="conteiner-fluid body_box">
+ <h1> Теория </h1>
+  <img src="Img\teoria.png"  width="512" height="288">
+</div>
+<?php require_once("footer.php");?>
