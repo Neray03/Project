cdadf91ddc53484aad9bd37760483cc4d6e61f30
Neray03 <82361112+Neray03@users.noreply.github.com>
2021-05-31 18:09:33 +0300

    Create fun.php
    Страничка "Расслабся)" - карусель из картинок.

---
+++ fun.php
@@ -0,0 +1,36 @@
+<?php require_once("header.php");?>
+<div class="conteiner-fluid body_box" >
+  <div class="conteiner-fluid carusel_box " >
+   <div id="carouselSlaids" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="Img\karusel7.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="Img\karusel2.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item w-100">
+        <img class="d-block w-100" src="Img\karusel3.jpg" alt="Third slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 " src="Img\karusel4.jpg" alt="Fourth slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 " src="Img\karusel5.png" alt="Fifth slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="Img\karusel6.jpg" alt="Sixth slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselSlaids" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselSlaids" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+<?php require_once("footer.php");?>
