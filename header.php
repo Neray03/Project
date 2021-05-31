b6f62b1d6bcb204fdf28e1a2f564505b2c65f9ec
Neray03 <82361112+Neray03@users.noreply.github.com>
2021-05-31 18:04:05 +0300

    Create header.php
    В этом файле прописано начало каждой страницы сайта. Он одинаков для всех страничек.

---
+++ header.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <script src="https://kit.fontawesome.com/8d9c1ffc52.js" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
+    <title>Задчи школьной физики</title>
+    <style>
+      .header_box {
+       
+        line-height: 100px;
+        width:inherit ;
+        background: url(Img/header.jpg);
+        background-size: cover;
+        opacity: 0,9;
+        text-align: center;
+      }
+      .menu_box {
+        line-height: 30px;
+        display: inline-block;
+        vertical-align: middle; 
+      }
+      .footer_box {
+       line-height: 100px;
+       background: url(Img/footer.jpg);
+       background-size: cover;
+       opacity: 0,5;
+       text-align: center;
+      }
+       .body_box {
+        height: 500px;
+        background: url(Img/main.jpg);
+        background-size: cover;
+        opacity: 0,1;
+        text-align:center;
+
+       }
+       .wait_box {
+        line-height: 30px;
+        display: inline-block; 
+        vertical-align: middle; 
+        background-color: white;
+       }
+       .carusel_box {
+        margin-top: 10px;
+        height: 480px;
+        width: 600px;
+        border: 1px solid black; /* Параметры рамки */
+        padding: 3px; /* Поля вокруг текста */
+        display: inline-block;; /* Устанавливаем как встроенный элемент */
+        background: #efecdf; /* Цвет фона */
+        align-content: center;
+       }
+       .lk_box{
+        height: 500px;
+        width: 200px;
+        border: 1px solid black; /* Параметры рамки */
+        padding: 3px; /* Поля вокруг текста */
+        background: #efecdf; /* Цвет фона */   
+        display: inline-block;
+       }   
+       
+   </style>
+ </head>
+<body>
+<header>
+  <div class="conteiner header_box" >
+    <div class="menu_box">
+     
+  <nav class="navbar navbar-expand-lg  navbar-light bg-light justify-content-center">
+   <a class="navbar-brand"  href="index.php"> <i class="far fa-atom"> Главная </i> </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Переключатель навигации">
+   </button>
+   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active"  href="taskmenu.php"> <i class="far fa-square-root-alt">Задачи </i><span class="sr-only">(текущая)</span></a>
+      <a class="nav-link"  href="teoria.php"> <i class="far fa-feather"> Теоретический материал </i></a>
+      <a class="nav-link"  href="lk.php"> <i class="far fa-user">Личный кабинет</i> </a>
+      <a class="nav-link" href="fun.php"> <i class="far fa-poo">  Расслабся)</i></a>
+    </div>
+   </div> 
+  </nav>
+</div>
+</header>
