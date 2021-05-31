af8bbe9843d117f6b9f8016e67c8d2994c7c1162
Neray03 <82361112+Neray03@users.noreply.github.com>
2021-05-31 18:11:27 +0300

    Create lk.php
    Cтраница личного кабинета

---
+++ lk.php
@@ -0,0 +1,17 @@
+<?php require_once("header.php");?>
+<div class="container-fluid body_box">
+    <div class="row">
+      <div class="col-md-5" align=left>
+        <img src="Img/albert.jpg" alt="Альберт Эйнштейн" height="200px" >
+        <h1>Имя : Альберт</h1>
+        <h1>Фамилия : Эйнштейн</h1>
+        <h1>Статус : Ученый</h1>
+        <h1>E-mail :einschtein@mail.ru</h1>
+      </div>
+      <div class="col-md-7 lk_box" mt-5>
+        <h1 class="my-5 text-center">Здесь будут ссылки на просмотренные задачи</h1>
+      </div>
+      
+    </div>
+</div>       
+<?php require_once("footer.php");?>
